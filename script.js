960745eaca3b6c1f38e57ee52c768a261cd07d25
razouck <razouck@users.noreply.github.com>
2023-01-09 16:32:46 -0300

    improve vertical alignment of the page
    Now the page conforms conforms to a proper vertical grid. One made of 12
    columns, each with a gap of 30px from the other ones.

--- script.js
+++ script.js
@@ -4,41 +4,80 @@ const COOL_RNG = rng(0, 9999999999);
 // other elements, at the cost of possibly creating a variable that the user may
 // already have in their environment... Hopefully they don't have a variable
 // called 'COOL_RNG'.
+// what if i styled the elements directly tho lmao
 
 function draw_grid(width, columns, gap)
 {
 	let style           = document.createElement('style');
 	let horizontal_grid = document.createElement('div');
+	let info            = document.createElement('div');
+
 	let css             = 
 		`
 		#horizontal_grid${COOL_RNG},
 		#horizontal_grid${COOL_RNG} div
 		{
-			outline          : none                 ;
-			border           : none                 ;
+			outline          : none                          ;
+			border           : none                          ;
 		}
 		#horizontal_grid${COOL_RNG}
 		{
-			display          : flex                 ;
-			justify-content  : space-between        ;
-			width            : ${width}px           ;
-			height           : 100vh                ;
-			position         : fixed                ;
-			top              : 0                    ;
-			left             : 50%                  ;
-			transform        : translateX(-50%)     ;
-			gap              : ${gap}px             ;
+			display          : flex                          ;
+			justify-content  : space-between                 ;
+			width            : ${width}px                    ;
+			height           : 100vh                         ;
+			position         : fixed                         ;
+			top              : 0                             ;
+			left             : 50%                           ;
+			transform        : translateX(-50%)              ;
+			gap              : ${gap}px                      ;
 		}
 		#horizontal_grid${COOL_RNG} div
 		{
 			flex-grow        : 1;
-			background-color : hsl(11 99% 72% / .2) ;
-			transition       : background-color .1s ;
+			background-color : hsl(11 99% 72% / .2)          ;
+			transition       : background-color .1s          ;
 		}
 		#horizontal_grid${COOL_RNG} div:hover,
 		#horizontal_grid${COOL_RNG} div.hover
 		{
-			background-color : hsl(11 99% 72% / .4) ;
+			background-color : hsl(11 99% 72% / .4)          ;
+		}
+		#info,
+		#info::after
+		{
+			position         : fixed                         ;
+			left             : 50%                           ;
+			transform        : translateX(-50%)              ;
+			border           : none                          ;
+			outline          : none                          ;
+			transform        : translateX(-50%)              ;
+			opacity          : 0                             ;
+			transition       : opacity 100ms                 ;
+		}
+		#info
+		{
+			top              : 100px                         ;
+			padding-left     : 1em                           ;
+			font-family      : verdana                       ;
+			font-size        : 10px                          ;
+			color            : #0081c9                       ;
+			border-bottom    : 1px solid #0081c9             ;
+			user-select      : none                          ;
+		}
+		#info::after
+		{
+			content          : ''                            ;
+			display          : block                         ;
+			bottom           : -6px                          ;
+			height           : 11px                          ;
+			width            : 100%                          ;
+		    box-shadow       : 1px 0 #0081c9, -1px 0 #0081c9 ;	
+		}
+		#info.visible,
+		#info.visible::after
+		{
+			opacity          : 100%                          ;
 		}
 		`;
 
@@ -65,17 +104,24 @@ function draw_grid(width, columns, gap)
 
 	function hightlightOppositeColumn(column)
 	{
-		getOppositeColumn(column.dataset.index).className = 'hover';
+		let column_index = column.dataset.index;
+		let width = getColumnDistance(column_index) + 'px';
+
+		getOppositeColumn(column_index).className = 'hover';
+		info.className = 'visible';
+		info.innerHTML = info.style.width = width;
 	}
 
 	function deemphasizeOppositeColumn(column)
 	{
 		getOppositeColumn(column.dataset.index).className = '';
+		info.className = '';
 	}
 
 	style.setAttribute('id', `sheet${COOL_RNG}`);
 	style.innerHTML = css;
 	document.head.appendChild(style);
+
 
 	horizontal_grid.setAttribute('id', `horizontal_grid${COOL_RNG}`);
 
@@ -97,6 +143,12 @@ function draw_grid(width, columns, gap)
 		);
 	}
 	document.body.appendChild(horizontal_grid);
+
+	info.innerHTML = '1320px'; // *
+	info.setAttribute('id', 'info');
+	document.body.appendChild(info);
+
+	onmousemove = e => info.style.top = e.y + 20 + 'px';
 }
 
 function repeat(str, n)
@@ -115,5 +167,10 @@ let width   = 1320;
 let columns = 12;
 let gap     = 30;
 
-draw_grid(width, columns, gap);
+const grid  = false;
+
+if (grid)
+{
+	draw_grid(width, columns, gap);
+}
 
