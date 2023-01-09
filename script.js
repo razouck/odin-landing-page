bceda84bbae89d2664bf241b7cc3e092a30aee3a
razouck <razouck@users.noreply.github.com>
2023-01-09 11:00:03 -0300

    add function to calculate column distance

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-function draw_grid(cols, gap)
+
+const COOL_RNG = rng(0, 9999999999);
+// By generating a unique id, this script is guaranteed to not interfere with any
+// other elements, at the cost of possibly creating a variable that the user may
+// already have in their environment... Hopefully they don't have a variable
+// called 'COOL_RNG'.
+
+function draw_grid(width, columns, gap)
 {
-	let horizontal_grid = document.createElement('div');
 	let style           = document.createElement('style');
-
-	horizontal_grid.setAttribute('id', 'horizontal_grid');
-
-	style.innerHTML =
+	let horizontal_grid = document.createElement('div');
+	let css             = 
 		`
-		#horizontal_grid,
-		#horizontal_grid div
+		#horizontal_grid${COOL_RNG},
+		#horizontal_grid${COOL_RNG} div
 		{
 			outline         : none;
 			border          : none;
 		}
-		#horizontal_grid
+		#horizontal_grid${COOL_RNG}
 		{
 			display         : flex;
 			justify-content : space-between;
-			width           : 1320px;
+			width           : ${width}px;
 			height          : 100vh;
 			position        : fixed;
 			top             : 0;
@@ -25,28 +29,72 @@ function draw_grid(cols, gap)
 			transform       : translateX(-50%);
 			gap             : ${gap}px;
 		}
-		#horizontal_grid div
+		#horizontal_grid${COOL_RNG} div
 		{
 			flex-grow       : 1;
-			background-color: hsl(0 100% 50% / .2);
+			background-color: hsl(11 99% 72% / .2);
+			transition      : background-color .1s;
 		}
-		#horizontal_grid div:hover,
-		#horizontal_grid div.hover
+		#horizontal_grid${COOL_RNG} div:hover,
+		#horizontal_grid${COOL_RNG} div.hover
 		{
-			background-color: hsl(110 100% 50% / .2);
+			background-color: hsl(11 99% 72% / .4);
 		}
 		`;
 
+	function getColumnDistance(index)
+	{
+		if (index > (columns / 2 - 1)) index = columns - 1 - index;
+		// Simplified formula tha returns the distance between the outer edges of
+		// two opposite columns.
+		return width - 2 * index * (width + gap) / columns;
+	}
+
+	function getOppositeColumn(index)
+	{
+		let n = columns - index;
+
+		console.log(getColumnDistance(index));
+
+		if (!n) return;
+
+		let selector = `div:nth-child(${n})`;
+
+		return horizontal_grid.querySelector(selector);
+	}
+
+	function hightlightOppositeColumn(column)
+	{
+		getOppositeColumn(column.dataset.index).className = 'hover';
+	}
+
+	function deemphasizeOppositeColumn(column)
+	{
+		getOppositeColumn(column.dataset.index).className = '';
+	}
+
+	style.setAttribute('id', `sheet${COOL_RNG}`);
+	style.innerHTML = css;
 	document.head.appendChild(style);
 
-	for (let i = 0; i < cols; i++)
-	{
-		let div = document.createElement('div');
+	horizontal_grid.setAttribute('id', `horizontal_grid${COOL_RNG}`);
 
-		div.setAttribute('data-index', i);
-		horizontal_grid.appendChild(div);
-		div.addEventListener('mouseenter', () => console.log(div.dataset.index));
-		div.addEventListener('mouseleave', () => console.log(':('));
+	for (let i = 0; i < columns; i++)
+	{
+		let column = document.createElement('div');
+
+		column.setAttribute('data-index', i);
+		horizontal_grid.appendChild(column);
+		column.addEventListener
+		(
+			'mouseenter',
+			() => hightlightOppositeColumn(column)
+		);
+		column.addEventListener
+		(
+			'mouseleave',
+			() => deemphasizeOppositeColumn(column)
+		);
 	}
 	document.body.appendChild(horizontal_grid);
 }
@@ -56,5 +104,16 @@ function repeat(str, n)
 	return Array(n + 1).join(str);
 }
 
-draw_grid(12, 30);
+function rng(min, max)
+{
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// ------------------------------------------------------------------------------
+
+let width   = 1320;
+let columns = 12;
+let gap     = 30;
+
+draw_grid(width, columns, gap);
 
